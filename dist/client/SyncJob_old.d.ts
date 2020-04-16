bef117d1e476687b83e5c9303b327b85ccb0e985
silas <silas@silas.link>
2020-04-16 10:42:55 +0200

    bugfix

---
+++ dist/client/SyncJob_old.d.ts
@@ -0,0 +1,27 @@
+export declare class SyncJob_old {
+    static SYNC_PATH_PREFIX: any;
+    _syncedModels: {};
+    _modelNames: any[];
+    _relationshipModels: {};
+    _lastSyncDates: {};
+    _keyedModelClasses: {};
+    _savePromises: any[];
+    _syncPromise: any;
+    syncInBackgroundIfDataExists(queries: any): Promise<void>;
+    getSyncPromise(): Promise<any>;
+    sync(queries: any): Promise<{}>;
+    private _doRuns;
+    private _handleRelations;
+    private _handleSingleRelation;
+    /**
+     * Extract the Entities and saves them(?) for one model
+     *
+     * @param modelRes
+     * @private
+     */
+    private _extractEntities;
+    private _buildRequestQuery;
+    private _getLastSyncModels;
+    private _addRelation;
+    static _fetchModel(query: any, offset: any): Promise<any>;
+}
