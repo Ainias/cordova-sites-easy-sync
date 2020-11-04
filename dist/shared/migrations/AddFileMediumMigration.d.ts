4adfb74744a070f6eaa80a6280d241521899cf2a
silas <silas@silas.link>
2020-11-04 14:21:19 +0100

    added fileMediumMigration

---
+++ dist/shared/migrations/AddFileMediumMigration.d.ts
@@ -0,0 +1,5 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class AddFileMediumMigration1000000011000 implements MigrationInterface {
+    down(queryRunner: QueryRunner): Promise<any>;
+    up(queryRunner: QueryRunner): Promise<any>;
+}
